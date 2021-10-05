b8300d845d007ddb67a172f92d099c79e34bd877
YuliaPol <62880025+YuliaPol@users.noreply.github.com>
2021-10-05 16:24:06 +0300

    add two block sections, add slider sections

---
+++ js/script.js
@@ -0,0 +1,36 @@
+jQuery(function ($) {
+    $(document).ready(function () {
+        $('.owl-carousel').owlCarousel({
+            items: 1,
+            margin: 35,
+            loop: true,
+            center: true,
+            stagePadding: 350,
+            nav: true,
+            lazyLoad: true,
+            autoHeight : false,
+            responsive : {
+                0: {
+                    stagePadding: 30,
+                    margin: 30,
+                },
+                451: {
+                    stagePadding: 40,
+                    margin: 35,
+                },
+                769: {
+                    stagePadding: 100,
+                    margin: 35,
+                },
+                1025: {
+                    stagePadding: 200,
+                    margin: 35,
+                },
+                1201: {
+                    stagePadding: 350,
+                    margin: 35,
+                }
+            },
+        });
+    });
+});
